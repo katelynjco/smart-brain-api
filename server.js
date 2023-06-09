b082953140f6bda5a73211c51364ea5f4663d9c0
Katelyn <developer@katelynj.co>
2023-06-09 16:30:12 -0400

    redo

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'password',
+      database : 'smartbrain'
+    }
+  });
+
+const app = express();
+const salt = bcrypt.genSaltSync(10);
+
+app.use(express.json());
+app.use(cors());
+
+// app.get('/', (req, res) => {
+//     res.send(database.users);
+// })
+
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, salt, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+
+
+app.listen(3005, () => {
+    console.log(`app is running on port 3005`);
+})
