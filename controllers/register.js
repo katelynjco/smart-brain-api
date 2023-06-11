6e3314ec33c8fec0a6ecf2548416cd9e07b3236d
Katelyn <developer@katelynj.co>
2023-06-11 17:36:53 -0400

    pls

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,8 @@
 
 const handleRegister = (req, res, db, salt, bcrypt) => {
     const { email, name, password } = req.body;
-    if (!email || !name || !password) {
-        console.log('Unable to register: Missing required fields');
-        return res.status(400).json('unable to register');
+    if( !email || !name || !password ) {
+         return res.status(400).json('unable to register');
     }
     const hash = bcrypt.hashSync(password, salt);
 
@@ -12,29 +11,24 @@ const handleRegister = (req, res, db, salt, bcrypt) => {
             hash: hash,
             email: email
         })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                console.log('Inserted into login table:', loginEmail[0].email);
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        email: loginEmail[0].email,
-                        name: name,
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        console.log('Inserted into users table:', user[0]);
-                        res.json(user[0]);
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-    })
-        .catch(err => {
-            console.log('Error occurred while registering:', err);
-            res.status(400).json('not able to register');
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0].email,
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
         })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('not able to register'))
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -10,11 +10,15 @@ const image = require('./controllers/image');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
+const PORT = process.env.PORT || 3005
+
 const db = knex({
     client: 'pg',
     connection: {
       connectionString : process.env.DATABASE_URL,
-      ssl : true,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
@@ -37,6 +41,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 
-app.listen(process.env.PORT || 3005, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+app.listen(PORT || 3005, () => {
+    console.log(`app is running on port ${PORT}`);
 })
